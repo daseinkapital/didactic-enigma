c1c46683ae18e7f576433bb297fa843621f7ae4e
Samuelson <591168@bah.com>
2017-07-31 09:10:09 -0400

    Initial demo commit

--- Scripts/predictive/predictive/urls.py
+++ Scripts/predictive/predictive/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^map/', include('map.urls'))
+    url(r'^map/', include('map.urls')),
+    url(r'^demo/', include('demo.urls'))
 ]
 
